f665acbf0acc5bfc31ddd2c780f8aa1d07a6416d
mayanksh99 <mayankshakya992@gmail.com>
2019-09-23 20:26:35 +0530

    Register Form Added

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -40,6 +40,11 @@ const NavBar = () => {
               Log In
             </NavLink>
           </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/register">
+              Register
+            </NavLink>
+          </li>
         </ul>
       </div>
     </nav>
